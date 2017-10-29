d4e54b49d460663e5ea17d7eb72f3579f5cbdfbb
Matthew Kram <mmazaxist9797@gmail.com>
2017-10-29 22:44:09 +0300

    AutoNext(Твой персональный дилер авто)

---
+++ assets/js/script.js
@@ -0,0 +1,192 @@
+$(document).ready(function(){
+$('.bxslider').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager'
+});
+});
+
+
+$(document).ready(function(){
+$('.bxslider2').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager2'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider3').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager3'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider4').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager4'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider5').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager5'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider6').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager6'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider7').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager7'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider8').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager8'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider9').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager9'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider10').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager10'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider11').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager11'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider12').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager12'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider13').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager13'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider14').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager14'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider15').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager15'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider16').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager16'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider17').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager17'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider18').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager18'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider19').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager19'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider20').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager20'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider21').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager21'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider22').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager22'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider23').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager23'
+});
+});
+
+$(document).ready(function(){
+$('.bxslider24').bxSlider({
+   nextText: '',
+   prevText: '',
+   pagerCustom: '#bx-pager24'
+});
+});
